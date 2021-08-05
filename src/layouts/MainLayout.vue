4f61a3acefc84f54231dd3a848385b1650b68b4f
Maashev Shamil <maashevshamil@gmail.com>
2021-08-05 19:41:08 +0600

    add changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,31 @@
 <template>
   <v-app id="inspire" class="overflow-y-none">
-    <v-navigation-drawer
-      v-model='drawer'
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6">
             Звоните по номеру:
           </v-list-item-title>
-          <v-list-item-subtitle>
-            +7(347)237-60-16
-          </v-list-item-subtitle>
+          <v-list-item-subtitle> +7(347)237-60-16 </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-            <router-link class="nav-link-item" :to="{ name: item.link }" >
-               <v-list-item-icon>
-                  <v-icon color="green darken-2">
-                     {{ item.icon }}
-                  </v-icon>
-               </v-list-item-icon>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <router-link class="nav-link-item" :to="{ name: item.link }">
+            <v-list-item-icon>
+              <v-icon color="green darken-2">
+                {{ item.icon }}
+              </v-icon>
+            </v-list-item-icon>
 
-               <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-               </v-list-item-content>
-            </router-link>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -58,44 +46,44 @@
       </template>
 
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      
+
       <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
 
-      
-      <template v-slot:extension v-if="$route.name === 'types' || $route.name === 'about'">
-        <v-tabs 
-            background-color="rgba(0,0,0,.3)" 
-            show-arrows 
-            grow 
-            align-with-title 
-            v-if="$route.name === 'types'"
-         >
-            <v-tab>Растениеводство</v-tab>
-            <v-tab>Животноводство и птицеводство</v-tab>
-            <v-tab>Лесное хозяйство</v-tab>
-            <v-tab>Здравоохранение</v-tab>
-            <v-tab>Мчс</v-tab>
+      <template
+        v-slot:extension
+        v-if="$route.name === 'types' || $route.name === 'about'"
+      >
+        <v-tabs
+          background-color="rgba(0,0,0,.3)"
+          show-arrows
+          grow
+          align-with-title
+          v-if="$route.name === 'types'"
+        >
+          <v-tab>Растениеводство</v-tab>
+          <v-tab>Животноводство и птицеводство</v-tab>
+          <v-tab>Лесное хозяйство</v-tab>
+          <v-tab>Здравоохранение</v-tab>
+          <v-tab>Мчс</v-tab>
         </v-tabs>
-        <v-tabs 
-            background-color="rgba(0,0,0,.3)" 
-            show-arrows 
-            grow 
-            align-with-title 
-            v-if="$route.name === 'about'"
-         >
-            <v-tab>О GARD</v-tab>
-            <v-tab>Преимущества GARD</v-tab>
-            <v-tab>Методики обработки</v-tab>
-            <v-tab>Наши достижения</v-tab>
+        <v-tabs
+          background-color="rgba(0,0,0,.3)"
+          show-arrows
+          grow
+          align-with-title
+          v-if="$route.name === 'about'"
+        >
+          <v-tab>О GARD</v-tab>
+          <v-tab>Преимущества GARD</v-tab>
+          <v-tab>Методики обработки</v-tab>
+          <v-tab>Наши достижения</v-tab>
         </v-tabs>
       </template>
-
     </v-app-bar>
 
-    <v-main
-    >
+    <v-main>
       <div class="main-content">
-         <router-view />
+        <router-view />
       </div>
     </v-main>
   </v-app>
@@ -103,118 +91,171 @@
 
 <script>
 export default {
-   created() {
-      if(window.innerWidth <= 450) {
-         this.drawer = false
+  created() {
+    if (window.innerWidth < 1263) {
+      this.drawer = false;
+    } else {
+      this.drawer = true;
+    }
+  },
+  data: () => ({
+    width: 0,
+    drawer: null,
+    items: [
+      { title: "Главная", icon: "mdi-home", link: "/" },
+      { title: "Виды обработок", icon: "mdi-sprout", link: "types" },
+      {
+        title: "Новости",
+        icon: "mdi-newspaper-variant-multiple-outline",
+        link: "news",
+      },
+      { title: "Фотографии", icon: "mdi-image", link: "photos" },
+      { title: "Видеозаписи", icon: "mdi-video", link: "videos" },
+      { title: "Партнеры", icon: "mdi-handshake-outline", link: "partners" },
+      { title: "Контакты", icon: "mdi-card-account-phone", link: "contacts" },
+      { title: "О компании", icon: "mdi-information-outline", link: "about" },
+    ],
+  }),
+  methods: {
+    updateWidth() {
+      this.width = window.innerWidth;
+    },
+  },
+  computed: {
+    navigationOpener() {
+      return this.width > 450;
+    },
+    pageTitle() {
+      if (this.$route.name === "types") {
+        return "Виды обработок";
+      } else if (this.$route.name === "news") {
+        return "Новости";
+      } else if (this.$route.name === "photos") {
+        return "Фотографии";
+      } else if (this.$route.name === "videos") {
+        return "Видеозаписи";
+      } else if (this.$route.name === "partners") {
+        return "Партнеры";
+      } else if (this.$route.name === "contacts") {
+        return "Контакты";
+      } else if (this.$route.name === "about") {
+        return "О компании";
       } else {
-         this.drawer = true
+        return "GARD - сервис";
       }
-   },
-   data: () => (
-   {
-      width: 0,
-      drawer: null,
-      items: [
-         { title: 'Главная', icon: 'mdi-home', link: '/' },
-         { title: 'Виды обработок', icon: 'mdi-sprout', link: 'types' },
-         { title: 'Новости', icon: 'mdi-newspaper-variant-multiple-outline', link: 'news' },
-         { title: 'Фотографии', icon: 'mdi-image', link: 'photos' },
-         { title: 'Видеозаписи', icon: 'mdi-video', link: 'videos' },
-         { title: 'Партнеры', icon: 'mdi-handshake-outline', link: 'partners' },
-         { title: 'Контакты', icon: 'mdi-card-account-phone', link: 'contacts' },
-         { title: 'О компании', icon: 'mdi-information-outline', link: 'about' },
-      ],
-   }),
-   methods: {
-      updateWidth() {
-         this.width = window.innerWidth;
-      },
-   },
-   computed: {
-      navigationOpener() {
-         return this.width > 450;
-      },
-      pageTitle () {
-         if (this.$route.name === 'types') {
-            return 'Виды обработок'
-         } else if (this.$route.name === 'news') {
-            return 'Новости'
-         } else if (this.$route.name === 'photos') {
-            return 'Фотографии'
-         } else if (this.$route.name === 'videos') {
-            return 'Видеозаписи'
-         } else if (this.$route.name === 'partners') {
-            return 'Партнеры'
-         } else if (this.$route.name === 'contacts') {
-            return 'Контакты'
-         } else if (this.$route.name === 'about') {
-            return 'О компании'
-         } else {
-            return 'GARD - сервис'
-         }
-      },
-      barSrc () {
-         if (this.$route.name === '/') {
-            return require('@/assets/img/6.jpg')
-         } else if (this.$route.name === 'about') {
-            return require('@/assets/img/4.jpg')
-         } else if (this.$route.name === 'types') {
-            return require('@/assets/img/2.jpg')
-         } else {
-            return require('@/assets/img/3.jpg')
-         }
+    },
+    barSrc() {
+      if (this.$route.name === "/") {
+        return require("@/assets/img/6.jpg");
+      } else if (this.$route.name === "about") {
+        return require("@/assets/img/4.jpg");
+      } else if (this.$route.name === "types") {
+        return require("@/assets/img/2.jpg");
+      } else {
+        return require("@/assets/img/3.jpg");
       }
-   }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@mixin laptop {
+  @media screen and (max-width: 1024px) {
+    @content;
+  }
+}
+@mixin tablets {
+  @media screen and (max-width: 768px) {
+    @content;
+  }
+}
+@mixin phones {
+  @media screen and (max-width: 425px) {
+    @content;
+  }
+}
+
+// Адаптивное свойство
+$maxWidth: 1920;
+$maxWidthContainer: 1170;
+
+@mixin adaptiv-value($property, $startSize, $minSize, $type) {
+  $addSize: $startSize - $minSize;
+  @if $type==1 {
+    // Только если меньше контейнера
+    #{$property}: $startSize + px;
+    @media (max-width: #{$maxWidthContainer + px} ) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} *
+          ((100vw - 320px) / #{$maxWidthContainer - 320})
+      );
+    }
+  } @else if $type==2 {
+    // Только если больше контейнера
+    #{$property}: $startSize + px;
+    @media (min-width: #{$maxWidthContainer + px}) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+      );
+    }
+  } @else {
+    // Всегда
+    #{$property}: calc(
+      #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+    );
+  }
+}
 .main-content {
   padding: 40px;
-  background: #EEF3FA;
-  font-family: 'DinPro', serif;
+  background: #eef3fa;
+  font-family: "DinPro", serif;
   font-size: 24px;
+  @include adaptiv-value("padding", 40, 15, 1);
+  @include adaptiv-value("padding-top", 40, 25, 1);
 }
 .theme--light.v-navigation-drawer {
-   background: #F9F9F9;
+  background: #f9f9f9;
 }
 .v-list-item__content {
   padding: 10px 0;
 }
 .v-slide-group__content {
   justify-content: space-around;
-  background: rgba(0,0,0,1);
+  background: rgba(0, 0, 0, 1);
 }
 .nav-link-item {
-   width: 100%;
-   display: flex;
-   align-items: center;
-   color: rgba(0, 0, 0, 0.6);
-   text-decoration: none;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.6);
+  text-decoration: none;
 }
 .v-list--nav .v-list-item {
-   padding: 0;
+  padding: 0;
 }
 .nav-link-item {
-   padding: 0 8px;
+  padding: 0 8px;
 }
 .router-link-exact-active {
-   transition: all .5s;
-   background: rgba(0, 0, 0, 0.1);
-   border-radius: 4px;
+  transition: all 0.5s;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
 }
-.v-slide-group__next, .v-slide-group__prev {
-   flex: 1 0 20px;
-   min-width: 20px;
+.v-slide-group__next,
+.v-slide-group__prev {
+  flex: 1 0 20px;
+  min-width: 20px;
 }
 @media screen and (max-width: 450px) {
-   .v-tab {
-      font-size: 0.675rem;
-      padding: 0 7px;
-   }
-   .v-slide-group__next, .v-slide-group__prev {
-      flex: 1 0 20px;
-      min-width: 20px;
-   }
+  .v-tab {
+    font-size: 0.675rem;
+    padding: 0 7px;
+  }
+  .v-slide-group__next,
+  .v-slide-group__prev {
+    flex: 1 0 20px;
+    min-width: 20px;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,82 +1,152 @@
 <template>
-<div class="home-page">
-  <div class="first-block-wrapper">
-    <div class="container">
-      <div class="section-title">Установка ГАРД</div>
-      <div class="first-block">
-        <div class="first-block__item first-block__left">
-          <p><span>Г</span>енератор</p>
-          <p><span>А</span>эрозольной</p>
-          <p><span>Р</span>егулируемой</p>
-          <p><span>Д</span>исперсности</p>
+  <div class="home-page">
+    <div class="first-block-wrapper">
+      <div class="container">
+        <div class="section-title">Компания «ГАРД Сервис»</div>
+        <div class="content-block about-block">
+          Мы занимаемся оказанием услуг при помощи установок ГАРД® собственного
+          производства. Мы не производим технику на продажу, поскольку считаем,
+          что в сфере аэрозольных технологий должны быть задействованы
+          исключительно специалисты, которые знают все тонкости работ, связанных
+          с применение пестицидов. Остерегайтесь лиц, действующих от нашего
+          имени. Нас Вы всегда можете найти по телефону: + 7 (347) 237-60-16. Мы
+          всегда рады Вам помочь!
         </div>
-        <div class="first-block__item first-block__right">
-          <p>
-            Это универсальная многооперационная установка, имеющая широкую область применения при использовании разрешенных химических и биологических препаратов за счет совмещения в одном механизме функций генератора аэрозоля регулируемой дисперсности частиц и дистанционного мелко, средне и крупнокапельного опрыскивателя.
-          </p>
-          <v-btn
-            x-large
-            color="success"
-            dark
-          >
-            Узнать больше ...
-          </v-btn>
+        <div class="section-title">Установка ГАРД</div>
+        <div class="first-block">
+          <div class="first-block__item first-block__left">
+            <p><span>Г</span>енератор</p>
+            <p><span>А</span>эрозольной</p>
+            <p><span>Р</span>егулируемой</p>
+            <p><span>Д</span>исперсности</p>
+          </div>
+          <div class="first-block__item first-block__right">
+            <p>
+              Это универсальная многооперационная установка, имеющая широкую
+              область применения при использовании разрешенных химических и
+              биологических препаратов за счет совмещения в одном механизме
+              функций генератора аэрозоля регулируемой дисперсности частиц и
+              дистанционного мелко, средне и крупнокапельного опрыскивателя.
+            </p>
+            <v-btn x-large color="success" dark> Узнать больше ... </v-btn>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-   <div class="second-block">
-     <div class="container">
+    <div class="second-block">
+      <div class="container">
         <div class="slider">
-          <v-carousel
-            show-arrows-on-hover
-            height='700px'
-          >
+          <v-carousel show-arrows height="700px">
             <v-carousel-item
-              v-for="(img,i) in images"
+              v-for="(img, i) in images"
               :key="i"
               :src="img.src"
             ></v-carousel-item>
           </v-carousel>
         </div>
-     </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data () {
-      return {
-        images: [
-          {
-            src: require('@/assets/img/gard5.jpg'),
-          },
-          {
-            src: require('@/assets/img/gard4.jpg'),
-          },
-          {
-            src: require('@/assets/img/gard.jpg'),
-          },
-        ],
-      }
-    },
-    components: {
-    },
-  }
+export default {
+  name: "Home",
+  data() {
+    return {
+      images: [
+        {
+          src: require("@/assets/img/gard5.jpg"),
+        },
+        {
+          src: require("@/assets/img/gard4.jpg"),
+        },
+        {
+          src: require("@/assets/img/gard.jpg"),
+        },
+      ],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style lang="scss" scoped>
+@mixin laptop {
+  @media screen and (max-width: 1024px) {
+    @content;
+  }
+}
+@mixin tablets {
+  @media screen and (max-width: 768px) {
+    @content;
+  }
+}
+@mixin phones {
+  @media screen and (max-width: 425px) {
+    @content;
+  }
+}
+
+$maxWidth: 1920;
+$maxWidthContainer: 1170;
+
+@mixin adaptiv-value($property, $startSize, $minSize, $type) {
+  $addSize: $startSize - $minSize;
+  @if $type==1 {
+    // Только если меньше контейнера
+    #{$property}: $startSize + px;
+    @media (max-width: #{$maxWidthContainer + px} ) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} *
+          ((100vw - 320px) / #{$maxWidthContainer - 320})
+      );
+    }
+  } @else if $type==2 {
+    // Только если больше контейнера
+    #{$property}: $startSize + px;
+    @media (min-width: #{$maxWidthContainer + px}) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+      );
+    }
+  } @else {
+    // Всегда
+    #{$property}: calc(
+      #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+    );
+  }
+}
+
+.content-block {
+  @include adaptiv-value("margin-bottom", 50, 20, 1);
+  @include adaptiv-value("font-size", 24, 14, 1);
+}
+
 .first-block {
   display: flex;
-  flex-wrap: wrap;
   margin-bottom: 50px;
+  @include tablets() {
+    flex-wrap: wrap;
+    margin-bottom: 30px;
+  }
   &__left {
-    margin-right: 100px;
+    margin-right: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    @include tablets() {
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
     p {
       font-size: 32px;
       font-weight: bold;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      @include adaptiv-value("font-size", 32, 22, 1);
+      @include adaptiv-value("margin-bottom", 16, 5, 1);
       span {
         color: rgb(24, 173, 24);
       }
@@ -85,18 +155,97 @@
   &__right {
     padding: 10px 0;
     font-size: 22px;
-    flex: 0 1 60%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    // flex: 0 1 60%;
+    @include adaptiv-value("font-size", 24, 14, 1);
+    @include tablets() {
+      flex: 0 1 100%;
+    }
+  }
+  .v-btn {
+    width: auto;
+    @include phones() {
+      width: 100%;
+      @include adaptiv-value("font-size", 16, 14, 1);
+    }
   }
 }
 
 .second-block {
+  .v-carousel {
+    height: calc(
+      200px + (500 - 200) * ((100vw - 320px) / (1200 - 320))
+    ) !important;
+  }
   .slider {
     position: relative;
     z-index: 1;
     overflow: hidden;
     border-radius: 5px;
     border: 2px solid #a4bbdd;
-    box-shadow: 0 0 7px 3px rgba(0,0,0,.2);
+    box-shadow: 0 0 7px 3px rgba(0, 0, 0, 0.2);
+  }
+}
+</style>
+
+<style lang="scss">
+$maxWidth: 1920;
+$maxWidthContainer: 1170;
+@mixin adaptiv-value($property, $startSize, $minSize, $type) {
+  $addSize: $startSize - $minSize;
+  @if $type==1 {
+    // Только если меньше контейнера
+    #{$property}: $startSize + px;
+    @media (max-width: #{$maxWidthContainer + px} ) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} *
+          ((100vw - 320px) / #{$maxWidthContainer - 320})
+      );
+    }
+  } @else if $type==2 {
+    // Только если больше контейнера
+    #{$property}: $startSize + px;
+    @media (min-width: #{$maxWidthContainer + px}) {
+      #{$property}: calc(
+        #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+      );
+    }
+  } @else {
+    // Всегда
+    #{$property}: calc(
+      #{$minSize + px} + #{$addSize} * ((100vw - 320px) / #{$maxWidth - 320})
+    );
+  }
+}
+.home-page {
+  .slider {
+    .v-carousel__item {
+      height: calc(
+        200px + (500 - 200) * ((100vw - 320px) / (1200 - 320))
+      ) !important;
+    }
+    .v-window__container {
+      height: calc(
+        200px + (500 - 200) * ((100vw - 320px) / (1200 - 320))
+      ) !important;
+    }
+    .v-carousel__controls {
+      height: calc(
+        30px + (50 - 30) * ((100vw - 320px) / (1200 - 320))
+      ) !important;
+    }
+    .v-btn__content i {
+      font-size: calc(
+        13px + (18 - 13) * ((100vw - 320px) / (1200 - 320))
+      ) !important;
+    }
+    .v-btn--icon.v-size--small {
+      @include adaptiv-value("width", 38, 18, 1);
+      @include adaptiv-value("height", 38, 18, 1);
+    }
   }
 }
 </style>
